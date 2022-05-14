338c45374a5cad2de4204534de175da5f53de0e5
rima34 <benkilanirim45@gmail.Com>
2022-05-14 11:47:17 +0100

    first commit

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app= express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use ('/api/goals',require('./routes/goalRoutes'))
+app.use ('/api/books',require('./routes/bookRoutes'))
 
 app.use(errorHandler)
 
